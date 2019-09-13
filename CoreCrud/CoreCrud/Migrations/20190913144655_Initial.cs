85042718d4ee65fd1d734f8e38e45fcb01fa01d1
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2019-09-13 18:14:45 -0400

    CoreCrud Commit

---
+++ CoreCrud/CoreCrud/Migrations/20190913144655_Initial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreCrud.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Destination",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<string>(nullable: true),
+                    TravelDate = table.Column<DateTime>(nullable: true),
+                    IsFlightServiceAvailable = table.Column<bool>(nullable: false),
+                    TotalFair = table.Column<decimal>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destination", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Destination_Country_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Country",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Destination_LocationId",
+                table: "Destination",
+                column: "LocationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destination");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
---
+++ CoreCrud/CoreCrud/Models/Destination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCrud.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ZipCode { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? TravelDate { get; set; }
+        public bool IsFlightServiceAvailable { get; set; }
+        public decimal TotalFair { get; set; }
+        public int LocationId { get; set; }
+        public Country Location { get; set; }
+
+    }
+}
