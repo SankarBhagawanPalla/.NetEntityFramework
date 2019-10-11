b1a88845ba78389e6cd6c407f6f935d3ce35fe44
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2019-10-11 15:34:24 -0400

    Frontend changes

--- LibrarySystem/LibrarySystem/Pages/Index.cshtml.cs
+++ LibrarySystem/LibrarySystem/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibrarySystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,19 @@ namespace LibrarySystem.Pages
 {
     public class IndexModel : PageModel
     {
+        private LibrarySystemContext _context;
+
+        public IndexModel(LibrarySystemContext context)
+        {
+            _context = context;
+        }
+        public ICollection<BorrowedBook> Borrowedbooks { get; set; }
         public void OnGet()
         {
+            Borrowedbooks = _context.BorrowedBook
+                                           .Where(x => x.BorrowedDate > new DateTime(2019, 1, 1, 0, 0, 0)
+                                           .ToList();
+                                            
 
         }
     }
